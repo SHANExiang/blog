dba6b5ba5908173ad0d35165eb84ca5640006a94
shaneDong <33693850+SHANExiang@users.noreply.github.com>
2020-07-02 22:44:19 +0800

    [myblog] admin注册

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from articles import models
 # Register your models here.
+
+admin.site.register(models.Classfication)
+admin.site.register(models.Article)
+admin.site.register(models.Tag)
